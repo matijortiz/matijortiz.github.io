b62e283b08278ebc712121b3fec2ee0977fb2870
Mati Ortiz <117781815+matijortiz@users.noreply.github.com>
2026-03-02 14:32:29 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://matijortiz.github.io',
+  site: 'https://matijortiz.com',
   integrations: [
     sitemap(),
   ],
